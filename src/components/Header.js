8baea015053133c09812c45c422f2483b521374c
arielEliasPerez <ariel65_ep@hotmail.com>
2024-10-09 02:22:03 -0300

    Header v1 - primera estructura

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,48 @@ import { Link } from "react-router-dom";
 import "./Header.css";
 
 import logo from '../assets/images/logo.png';
+import { FaFacebook, FaInstagram, FaWhatsapp } from "react-icons/fa";
 
 const Header = () => {    
     return (
         <header>
-        <img className="logo-img" src={logo} alt="Logo Mitos" />
+        <div className="header-container">
+            <nav className="social-links">
+            <a
+                    href="https://www.instagram.com/mitos.cerveceria"
+                    target="_blank"
+                    aria-label="Instagram"
+                >
+                    <FaInstagram className="social-icon" />
+                </a>
+                <a
+                    href="https://www.facebook.com/profile.php?id=100049956774559"
+                    target="_blank"
+                    aria-label="Facebook"
+                >
+                    <FaFacebook className="social-icon" />
+                </a>
+            </nav>
+            <div className="logo-container">
+                <img className="logo-img" src={logo} alt="Logo Mitos" />
+                <p>-•- RESTO BAR -•-</p>
+            </div>
+            <nav className="social-links">
+                
+                <a
+                    className="whatsapp-icon"
+                    href="https://api.whatsapp.com/send/?phone=541135887741&text=X+reserva&type=phone_number&app_absent=0"
+                    target="_blank"
+                    aria-label="Whatsapp"
+                >
+                    <FaWhatsapp /> +549 1135887741
+                </a>
+            </nav>
+        </div>
+        
         <div className="navbar-container">
             <nav>
-                <ul>
+                <ul className="sections-list">
                     <li><Link to="/">Inicio</Link></li>
                     <li><Link to="/menu">Menú</Link></li>
                     <li><Link to="/gallery">Fotos</Link></li>
